a4a63694c008b828680cec779f2b26588ee5a96d
mrylraihan <mr.ylraihan@gmail.com>
2023-05-01 21:13:32 -0400

    just incase

--- TestingWithJest/Day10MockingDataCoda/Test/app.test.js
+++ TestingWithJest/Day10MockingDataCoda/Test/app.test.js
@@ -1,7 +1,7 @@
 const index = require('../app')
 
 describe('exponent numbers test',()=>{
-    
+    // beforeEach(()=>jest.resetAllMocks())
     it('should return the result of the number raised to the exponent',()=>{
         jest.spyOn(index, 'multipleNumbers').mockImplementation((x,y)=>x*y)
         // arrange
@@ -24,13 +24,26 @@ describe('exponent numbers test',()=>{
         // assert
         expect(result).toBe(13)
     })
+    // it will use the exponentNumbers Function that was last mocked, with out resetting the mock in the before or after Each it will use the previous mocked as the functions definition
+    it('should return the result of the number raised to the sum',()=>{
+  
+        // arrange
+        const num = 3;
+        const exponent = 4;
+
+        // act
+        const result = index.exponentNumbers(num, exponent)
+        // assert
+        expect(result).toBe(13)
+        // expect(result).toBe(-11)if we have the before each resetting each mock
+    })
 })
 
 describe('exponent numbers test',()=>{
     const mockAdd = jest.spyOn(index, 'addNumbers').mockImplementation((x,y)=>x+y)
 
     beforeEach(()=>mockAdd.mockClear())
-    // afterAll(()=>mockAdd.mockRestore())
+    afterAll(()=>mockAdd.mockRestore())
     it('should return the result of the number raised to the exponent',()=>{
         // arrange
         const num1 = 1;
